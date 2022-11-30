2f41dbf03514ec8c63c6eee458654117b71be72b
1988db <elozrana@gmail.com>
2022-11-30 22:23:18 +0100

    feat: add mobile menu to donate and main pages

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -1,6 +1,20 @@
-/*document.addEventListener('DOMContentLoaded', ()=> {
-    const submit = document.getElementById('submit');
-    submit.addEventListener('click', (e)=> {
-        e.preventDefault();
-    })
-})*/
+document.addEventListener('DOMContentLoaded', ()=> {
+    const burgerBtn = document.getElementById('burger-btn');
+    const header = document.querySelector('header');
+    const logo = document.getElementById('landing');
+    const navigation = document.getElementById('navigation');    
+    burgerBtn.addEventListener('click', (e)=> {
+        header.classList.toggle('visible');
+        e.stopPropagation();
+    });
+    header.addEventListener('click', ()=> {
+        header.classList.remove('visible');
+    });
+    logo.
+    addEventListener('click', (e)=> {        
+        e.stopPropagation();
+    });
+    navigation.addEventListener('click', (e)=> {
+        e.stopPropagation();
+    });    
+})
--- pages/main/script.js
+++ pages/main/script.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log('DOM Content Loaded');
+    const burgerBtn = document.getElementById('burger-btn');
+    const header = document.querySelector('header');
+    const logo = document.getElementById('landing');
+    const navContainer = document.getElementById('nav-container');
+    const designedBy = document.getElementById('designed-by')
+    burgerBtn.addEventListener('click', (e)=> {
+        header.classList.toggle('visible');
+        e.stopPropagation();
+    });
+    header.addEventListener('click', ()=> {
+        header.classList.remove('visible');
+    });
+    logo.
+    addEventListener('click', (e)=> {        
+        e.stopPropagation();
+    });
+    navContainer.addEventListener('click', (e)=> {
+        e.stopPropagation();
+    });
+    designedBy.addEventListener('click', (e)=> {
+        e.stopPropagation();
+    });
 })
