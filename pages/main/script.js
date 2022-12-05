89a118a89d2ac8f28bdde69ecbf3d318636c095a
1988db <elozrana@gmail.com>
2022-12-05 12:23:15 +0100

    feat: add event listeners to arrows

--- pages/main/script.js
+++ pages/main/script.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const header = document.querySelector('header');
     const logo = document.getElementById('landing');
     const navContainer = document.getElementById('nav-container');
-    const designedBy = document.getElementById('designed-by')
+    const designedBy = document.getElementById('designed-by');    
+    const leftBtn = document.getElementById('arrow-lft-btn');
+    const rightBtn = document.getElementById('arrow-rgt-btn');
+    const viewportSize = document.getElementById('viewport');
+  
+    getViewportWidth();
+
+    window.addEventListener('resize', getViewportWidth);
+
     burgerBtn.addEventListener('click', (e)=> {
         header.classList.toggle('visible');
         e.stopPropagation();
@@ -21,4 +29,14 @@ document.addEventListener('DOMContentLoaded', ()=> {
     designedBy.addEventListener('click', (e)=> {
         e.stopPropagation();
     });
+
+    leftBtn.addEventListener('click', thumbailsCarouselLft);
+    rightBtn.addEventListener('click', thumbailsCarouselRgt);
+
+    function getViewportWidth () {
+        vieportWidth = window.innerWidth;
+        viewportSize.innerText = vieportWidth;
+    }   
+
+
 })
